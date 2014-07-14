fd87a7b65929102257ea78e95c9f9538c90964e6
pengshen001 <peng.shen@indochino.com>
2014-07-13 22:44:24 -0700

    full suite test script

---
+++ test/full_suite.sh
@@ -0,0 +1,12 @@
+cd account
+php runner.php config/full_suite.inc
+cd ..
+cd business
+php runner.php config/full_suite.inc
+cd ..
+cd comment
+php runner.php config/full_suite.inc
+cd ..
+cd image
+php runner.php config/full_suite.inc
+cd ..
