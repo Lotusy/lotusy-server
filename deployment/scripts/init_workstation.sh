6a2866219b990492fa1327e79958602505b49e8b
pengshen001 <peng.shen@indochino.com>
2014-09-24 11:37:53 -0700

    add init script and gitignore

---
+++ deployment/scripts/init_workstation.sh
@@ -0,0 +1,42 @@
+cd ../../account/dao/config/
+ln -s config.inc.dev config.inc
+cd ../../rest/config/
+ln -s config.inc.dev config.inc
+cd ../../portal/config/
+ln -s config.inc.dev config.inc
+cd ../..
+mkdir logs
+chmod 777 logs
+cd ../business/dao/config/
+ln -s config.inc.dev config.inc
+cd ../../rest/config/
+ln -s config.inc.dev config.inc
+cd ../../portal/config/
+ln -s config.inc.dev config.inc
+cd ../..
+mkdir logs
+chmod 777 logs
+cd ../comment/dao/config/
+ln -s config.inc.dev config.inc
+cd ../../rest/config/
+ln -s config.inc.dev config.inc
+cd ../../portal/config/
+ln -s config.inc.dev config.inc
+cd ../..
+mkdir logs
+chmod 777 logs
+cd ../image/dao/config/
+ln -s config.inc.dev config.inc
+cd ../../rest/config/
+ln -s config.inc.dev config.inc
+cd ../../portal/config/
+ln -s config.inc.dev config.inc
+cd ../..
+mkdir logs
+chmod 777 logs
+cd ../www/config
+ln -s config.inc.dev config.inc
+cd ..
+mkdir logs
+chmod 777 logs
+
