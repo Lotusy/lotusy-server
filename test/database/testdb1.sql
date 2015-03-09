3a22c9d3cf472c4d287e4797d7d6aee8ebfa8645
pengshen001 <peng.shen@indochino.com>
2015-03-08 23:23:43 -0700

    add testing db file

---
+++ test/database/testdb1.sql
@@ -0,0 +1,22 @@
+-- user table
+INSERT INTO foodster.user 
+(external_type,external_ref,email,password,username,nickname,gender,rank,profile_pic,description,last_login,superuser,blocked)
+VALUES 
+('1','1537102412',NULL,NULL,'TestUser0','testuser0',NULL,'http://www.facebook.com/2075018','',NULL,'N','N',NULL),
+('2','1537112412',NULL,NULL,'TestUser1','testuser1',NULL,'http://www.facebook.com/2071235018','',NULL,'N','N',NULL),
+('1','1537122412',NULL,NULL,'TestUser2','testuser2',NULL,'http://www.facebook.com/2075141518','',NULL,'N','N',NULL),
+('2','1537132412',NULL,NULL,'TestUser3','testuser3',NULL,'http://www.facebook.com/2073246235018','',NULL,'N','N',NULL),
+('1','1537142412',NULL,NULL,'TestUser4','testuser4',NULL,'http://www.facebook.com/20724565018','',NULL,'N','N',NULL),
+('2','1537152412',NULL,NULL,'TestUser5','testuser5',NULL,'http://www.facebook.com/207724575018','',NULL,'N','N',NULL),
+('1','1537162412',NULL,NULL,'TestUser6','testuser6',NULL,'http://www.facebook.com/2075234018','',NULL,'N','N',NULL),
+('2','1537172412',NULL,NULL,'TestUser7','testuser7',NULL,'http://www.facebook.com/207234625018','',NULL,'N','N',NULL),
+('1','1537182412',NULL,NULL,'TestUser8','testuser8',NULL,'http://www.facebook.com/207234565018','',NULL,'N','N',NULL),
+('2','1537192412',NULL,NULL,'TestUser9','testuser9',NULL,'http://www.facebook.com/2072365018','',NULL,'N','N',NULL);
+
+-- follower table
+
+-- dish table
+
+-- 
+
+-- dish table
